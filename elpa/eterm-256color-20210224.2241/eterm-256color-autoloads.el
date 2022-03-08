858f872703bc00bd6f67a57ffc1b59a41438474b
zvonkok <zkaiser@nvidia.com>
2022-03-08 20:45:02 +0100

    Initial commit

---
+++ elpa/eterm-256color-20210224.2241/eterm-256color-autoloads.el
@@ -0,0 +1,46 @@
+;;; eterm-256color-autoloads.el --- automatically extracted autoloads  -*- lexical-binding: t -*-
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "eterm-256color" "eterm-256color.el" (0 0 0
+;;;;;;  0))
+;;; Generated autoloads from eterm-256color.el
+
+(autoload 'eterm-256color-mode "eterm-256color" "\
+Minor mode that adds 256color support to term/ansi-term.
+
+This is a minor mode.  If called interactively, toggle the
+`Eterm-256color mode' mode.  If the prefix argument is positive,
+enable the mode, and if it is zero or negative, disable the mode.
+
+If called from Lisp, toggle the mode if ARG is `toggle'.  Enable
+the mode if ARG is nil, omitted, or is a positive number.
+Disable the mode if ARG is a negative number.
+
+To check whether the minor mode is enabled in the current buffer,
+evaluate `eterm-256color-mode'.
+
+The mode's hook is called both when the mode is enabled and when
+it is disabled.
+
+\(fn &optional ARG)" t nil)
+
+(register-definition-prefixes "eterm-256color" '("eterm-256color-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("eterm-256color-pkg.el") (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8-emacs-unix
+;; End:
+;;; eterm-256color-autoloads.el ends here
